c1f72c1ddd82273691680ddad960a09b22ab8402
Patryk1998 <patron4@onet.pl>
2018-01-11 17:37:07 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //calculator test
+
+        Calculator test = new Calculator();
+        int a=11, b=6;
+        int additionResult = test.add(a,b);
+        int subtractionResult = test.subtract(a,b);
+
+        if(additionResult==a+b && subtractionResult==a-b)
+        {
+            System.out.println("test OK");
+        } else System.out.println("Something is wrong!");
+
     }
 }
