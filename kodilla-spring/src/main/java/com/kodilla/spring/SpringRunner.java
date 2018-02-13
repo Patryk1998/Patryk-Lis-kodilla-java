a2b3180fb3e696c49dbe5338a06593c675e5c428
“Patryk1998 <patron4@onet.pl>
2018-02-13 15:13:03 +0100

    10.3 Module

--- kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
@@ -1,11 +1,12 @@
 package com.kodilla.spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringRunner {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRunner.class, args);
 	}
