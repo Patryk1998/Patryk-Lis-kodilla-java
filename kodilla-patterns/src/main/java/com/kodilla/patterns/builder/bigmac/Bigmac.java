2a9739a1d347d3e424a18be8ef95d5436b51496a
“Patryk1998 <patron4@onet.pl>
2018-03-02 18:51:07 +0100

    11.5 Exercise

--- kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
@@ -1,4 +1,95 @@
 package com.kodilla.patterns.builder.bigmac;
 
-public class Bigmac {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Bigmac {
+    private final String roll;
+    private final String burgers;
+    private final String sauce;
+    private final List<String> ingredients;
+
+    public static class BigmacBuilder {
+        private String roll;
+        private String burgers;
+        private String sauce;
+        private List<String> ingredients = new ArrayList<>();
+        final static String withSesame = "Sesame";
+        final static String noSesame = "noSesame";
+        final static String standard = "standard";
+        final static String barbecue = "barbecue";
+        final static String cheese = "cheese";
+        final static String onion = "onion";
+        final static String cucumber = "cucumber";
+        final static String one = "one";
+        final static String two = "two";
+
+
+        public BigmacBuilder roll(String roll) {
+            if (roll.equals(withSesame) || roll.equals(noSesame)) {
+                this.roll = roll;
+            }
+            return this;
+        }
+
+        public BigmacBuilder burgers(String burgers) {
+            if (burgers.equals(one) || burgers.equals(two)) {
+                this.burgers = burgers;
+            }
+            return this;
+        }
+
+        public BigmacBuilder sauce(String sauce) {
+            if (sauce.equals(standard) || sauce.equals(barbecue)) {
+                this.sauce = sauce;
+            }
+            return this;
+        }
+
+        public BigmacBuilder ingredients(String ingredient) {
+            if (ingredient.equals(onion) || ingredient.equals(cheese) || ingredient.equals(cucumber)) {
+                ingredients.add(ingredient);
+            }
+            return this;
+        }
+
+        public Bigmac build() {
+            return new Bigmac(roll, burgers, sauce, ingredients);
+        }
+    }
+
+    private Bigmac(final String roll, final String burgers, final String sauce, final List<String> ingredients) {
+        this.roll = roll;
+        this.burgers = burgers;
+        this.sauce = sauce;
+        this.ingredients = ingredients;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public String getBurgers() {
+        return burgers;
+    }
+
+    public String getSauce() {
+        return sauce;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    @Override
+    public String toString() {
+        return "Bigmac{" +
+                "roll='" + roll + '\'' +
+                ", burgers=" + burgers +
+                ", sauce='" + sauce + '\'' +
+                ", ingredients=" + ingredients +
+                '}';
+    }
 }
+
+
--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -1,4 +1,26 @@
 package com.kodilla.patterns.builder.bigmac;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static com.kodilla.patterns.builder.bigmac.Bigmac.BigmacBuilder.*;
+
 public class BigmacTestSuite {
+    @Test
+    public void testBigmacBuild() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .ingredients(cheese)
+                .sauce(standard)
+                .burgers(two)
+                .ingredients(onion)
+                .roll(noSesame)
+                .build();
+        //When
+        System.out.println(bigmac);
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(2, howManyIngredients);
+
+    }
 }
