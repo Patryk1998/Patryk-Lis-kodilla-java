486ea4f09397135c377f1045d4234d741a287ea7
PatrykLis <patryklis27@gmail.com>
2018-05-21 19:50:17 +0200

    23.4 Exercise

--- kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
+++ kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
@@ -33,7 +33,7 @@ public class CrudAppTestSuite {
         driver.close();
     }
 
-    private String createCrudAppTestTask() throws InterruptedException {
+    private String createCrudAppTestTask() {
         final String XPATH_TASK_NAME = "//form[contains(@action, \"createTask\")]/fieldset[1]/input";
         final String XPATH_TASK_CONTENT = "//form[contains(@action, \"createTask\")]/fieldset[2]/textarea";
         final String XPATH_ADD_BUTTON = "//form[contains(@action, \"createTask\")]/fieldset[3]/button";
@@ -46,9 +46,11 @@ public class CrudAppTestSuite {
         WebElement content = driver.findElement(By.xpath(XPATH_TASK_CONTENT));
         content.sendKeys(taskContent);
 
+
         WebElement addButton = driver.findElement(By.xpath(XPATH_ADD_BUTTON));
         addButton.click();
-        Thread.sleep(15000);
+
+        while (!driver.findElement(By.xpath("//form[@class=\"datatable__row\"]")).isDisplayed());
 
         return taskName;
     }
@@ -92,17 +94,18 @@ public class CrudAppTestSuite {
         driverTrello.findElement(By.id("password")).sendKeys("kodilla123");
         driverTrello.findElement(By.id("login")).submit();
 
-        Thread.sleep(3000);
+        Thread.sleep(4000);
 
         driverTrello.findElement(By.xpath("//a[@href=\"/patryklis/boards\"]")).click();
 
-        Thread.sleep(5000);
+        Thread.sleep(2000);
+
 
         driverTrello.findElements(By.xpath("//a[@class=\"board-tile\"]")).stream()
                 .filter(aHref -> aHref.findElements(By.xpath(".//span[@title=\"Kodilla Application\"]")).size() > 0)
                 .forEach(WebElement::click);
 
-        Thread.sleep(5000);
+        Thread.sleep(3000);
 
         try {
             result = driverTrello.findElements(By.xpath("//span")).stream()
